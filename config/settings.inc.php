d226ae1770f460b462cd734e1c5451a74d8cb6e2
Sam Fisher <samfisher8456@hotmail.com>
2015-03-23 21:07:03 +0100

    initialisation de git et ajout du projet au git

---
+++ config/settings.inc.php
@@ -0,0 +1,18 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop_test');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', 'azerty2121');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_MEDIA_SERVER_1_', '');
+define('_MEDIA_SERVER_2_', '');
+define('_MEDIA_SERVER_3_', '');
+define('_COOKIE_KEY_', 'FbiYDiPr85maSbxIaS490ZfP1g5RIbL3Ai2c3wdsgNOKdFT5R7gNqY4E');
+define('_COOKIE_IV_', '2QjNv8K0');
+define('_PS_CREATION_DATE_', '2015-03-23');
+define('_PS_VERSION_', '1.5.6.2');
+define('_RIJNDAEL_KEY_', 'yOHxIe4i1eMsEfwvhxmfML13rMX6skk0');
+define('_RIJNDAEL_IV_', 'czpaeqzqz2Rh1XWK2WJSFw==');
